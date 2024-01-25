db946ea6bdaeb1dbd207f54ae9aa2f1ecb825037
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-01-25 13:56:39 +0000

    Colocar nome user

--- Pages/script.js
+++ Pages/script.js
@@ -1,13 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var usertext = document.getElementById("usertext1");
+    var btn1 = document.getElementById("btn_login1");
+    var userHeader = document.getElementById("userHeader");
 
-const txt1 = document.getElementById('user1');
-const btn1 = document.getElementById('btn_lg1');
-const out1 = document.getElementById('user2');
+    function login() {
+        sessionStorage.setItem("username", usertext.value);
+        userHeader.innerHTML = "Welcome, " + sessionStorage.getItem("username") + "!";
+    }
 
+    if (btn1) {
+        btn1.addEventListener('click', login);
+    }
 
-function login() {
-    const username = txt1.value;
-    localStorage.setItem('username', username);
-    window.location.href = 'Pages/scrum.html';
-}
-btn1.addEventListener('click',login);
-    
+    // Set the username in the header if it exists in localStorage
+    var storedUsername = sessionStorage.getItem("username");
+    if (storedUsername) {
+        userHeader.innerHTML = "Welcome, " + storedUsername + "!";
+    }
+});
