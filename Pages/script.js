808ab14eb486f39f995eab8b7c4e56b83b106c22
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-01-25 22:45:16 +0000

    Teste de criar um Map

--- Pages/script.js
+++ Pages/script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var usertext = document.getElementById("usertext1");
-    var btn1 = document.getElementById("btn_login1");
-    var userHeader = document.getElementById("userHeader");
+    const usertext = document.getElementById("usertext1");
+    const btn1 = document.getElementById("btn_login1");
+    const userHeader = document.getElementById("userHeader");
 
     function login() {
         sessionStorage.setItem("username", usertext.value);
-        userHeader.innerHTML = "Welcome, " + sessionStorage.getItem("username") + "!";
+        userHeader.innerHTML = "Welcome, " + sessionStorage.getItem("username");
     }
 
     if (btn1) {
@@ -13,8 +13,30 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Set the username in the header if it exists in localStorage
-    var storedUsername = sessionStorage.getItem("username");
+    const storedUsername = sessionStorage.getItem("username");
     if (storedUsername) {
-        userHeader.innerHTML = "Welcome, " + storedUsername + "!";
+        userHeader.innerHTML = "Welcome, " + storedUsername;
     }
 });
+
+// Create a Map of tasks
+let tasks = new Map();
+document.getElementById("btn_submit").onclick = new_task;
+
+function new_task(){
+    let title_new = document.getElementById("title").value;
+    let description_new = document.getElementById("description").value;
+
+    tasks.set(title_new, description_new);
+    const list1 = document.createElement("li");
+    list1.innerText = title_new;
+
+    const todoList = document.getElementById("todo");
+    todoList.appendChild(list1);
+
+    // Clear the input fields after adding the task
+    document.getElementById("title").value = "";
+    document.getElementById("description").value = "";
+
+    window.location.href = 'scrum.html';
+}
