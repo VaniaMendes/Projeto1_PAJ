38110cd4d3a5ef01c4bc9edd847545c021187645
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-01-26 10:51:28 +0000

    Criar diferentes js

--- Pages/index.js
+++ Pages/index.js
@@ -18,25 +18,3 @@ document.addEventListener('DOMContentLoaded', function() {
         userHeader.innerHTML = "Welcome, " + storedUsername;
     }
 });
-
-// Create a Map of tasks
-let tasks = new Map();
-document.getElementById("btn_submit").onclick = new_task;
-
-function new_task(){
-    let title_new = document.getElementById("title").value;
-    let description_new = document.getElementById("description").value;
-
-    tasks.set(title_new, description_new);
-    const list1 = document.createElement("li");
-    list1.innerText = title_new;
-
-    const todoList = document.getElementById("todo");
-    todoList.appendChild(list1);
-
-    // Clear the input fields after adding the task
-    document.getElementById("title").value = "";
-    document.getElementById("description").value = "";
-
-    window.location.href = 'scrum.html';
-}
