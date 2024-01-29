cd749c04e6bf08ca6177043606211a6a62c8a386
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-01-29 10:14:02 +0000

    Revisão geral ao código e formatação página tarefa

--- Pages/scrum.js
+++ Pages/scrum.js
@@ -8,17 +8,14 @@ document.getElementById("userHeader").innerHTML = "Bem vindo, " + username;
 const btn_in = document.getElementById("btn_inicio");
 btn_in.onclick = homeMenu;
 
-//Quando clica no botao Inicio
+// When press "btn_inicio"
 function homeMenu(){
-    // localStorage.clear();        [VC] Acho que esta linha fica melhor substituída pela seguinte
     localStorage.removeItem("username");
     document.location.href = '../index.html';
 }  
 
-
 const btnNewTask = document.getElementById("btn_criar");
 btnNewTask.onclick = taskMenu;
-
 
 //Todas as tarefas que faz quando entra na segunda página
 function taskMenu(){
@@ -32,8 +29,7 @@ function taskMenu(){
             // Se existirem tarefas no localStorage, converte a string JSON de volta para um array de objetos
             tasks = JSON.parse(storedTasks);
 
-        
-            // Exemplo: renderiza as tarefas ao carregar a página (substitua por sua lógica)    [VC] Falta editar aqui alguma coisa?
+                    // Exemplo: renderiza as tarefas ao carregar a página (substitua por sua lógica)    [VC] Falta editar aqui alguma coisa?
             renderTasksOnPage();
         }
     });
@@ -42,10 +38,7 @@ function taskMenu(){
 }
 
 
-// window.onload = () =>{       [VC] Acho que este comando substitui o: document.addEventListener('DOMContentLoaded', function() { e fica como os slides da prof pg.74
-// };
-
-document.addEventListener('DOMContentLoaded', function() {
+window.onload = () =>{
 
     // Carregar as tarefas existentes do localStorage, se houver
     let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
@@ -62,13 +55,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Iterar sobre as tarefas e adicioná-las aos quadros apropriados
         for (const t of tasks){
-        // tasks.forEach(task => {
-            // const cardElement = createCardElement(task.title);
+        
             const cardElement = createCardElement(t.title);
-            // const columnElement = document.getElementById(task.column);
+            
             const columnElement = document.getElementById(t.column);
             columnElement.appendChild(cardElement);
-        // });
+        
         }
     }
 
@@ -82,10 +74,4 @@ document.addEventListener('DOMContentLoaded', function() {
     `;
         return cardElement;
     }
-});
-
-
-
-
-
-
+};
--- Pages/task.js
+++ Pages/task.js
@@ -8,7 +8,6 @@ document.getElementById("userHeader").innerHTML = "Bem vindo, " + username;
 // Carregar as tarefas existentes do localStorage, se existir um array senao cria um novo
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-// 
 const submitButton = document.getElementById("btn_submit");
 submitButton.onclick = add_task;
 
@@ -17,38 +16,16 @@ submitButton.onclick = add_task;
 //Adiciona uma nova task
 function add_task(){
 
-    document.location.href = 'scrum.html';
-   
-    //Gera um novo id sempre que seja criada uma tarefa
-    function generateID(){
-
-        return tasks.length + 1;
-    }
-
-    const newId = generateID();
-
-     
-
-    // console.log(newId); [VC] - verificar se é para apagar
     const titleInput = document.getElementById('title1');
-
-
-    // [VC] - tentar implementar lógica de não permite titulo em branco
-    if(titleInput === ""){
-        alert("Por favor preencha todos os campos.");
-        return;  
-    } 
-
     const descriptionInput = document.getElementById('description1');
 
     // Obtém os valores dos inputs
     const title = titleInput.value;
     const description = descriptionInput.value;
 
-
     // Create a new task object
     const newTask = {
-        id: newId,
+        id: tasks.length + 1,
         title: title,
         column: 'todo-cards',
         description: description
@@ -58,44 +35,16 @@ function add_task(){
     tasks.push(newTask);
     newTask.column ='todo-cards';
     localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    if(titleInput.value === ""){
+        alert("Por favor preencha todos os campos.");
+    } 
+    else{
+        alert('Nova tarefa adicionada com sucesso: ' + '"'  + newTask.title + '"');
+        document.location.href = 'scrum.html';
+    }
+
     // Limpar os campos após adicionar uma nova tarefa
     titleInput.value = "";
     descriptionInput.value = "";
-
-    alert('Nova tarefa adicionada com sucesso: ' + '"'  + newTask.title + '"');
-    
-   // listTasks();
 }
-
-
-/*
-
-function listTasks(){
-    // Limpar os quadros antes de listar novamente
-    document.getElementById("todo-cards").innerHTML = "";
-    document.getElementById("doing-cards").innerHTML = "";
-    document.getElementById("done-cards").innerHTML = "";
-
-
-    // Iterar sobre as tarefas e adicioná-las aos quadros apropriados
-    tasks.forEach(task => {
-        const cardElement = createCardElement(task.title);
-        const columnElement = document.getElementById(task.column);
-        columnElement.appendChild(cardElement);
-    });
-
-}
-// Função para criar um elemento de cartão HTML para uma tarefa
-function createCardElement(title) {
-    const cardElement = document.createElement("div");
-    cardElement.className = "card";
-    cardElement.draggable = true;
-    cardElement.innerHTML = `
-        <div class="card-header">${title}</div>
-
-    `;
-    return cardElement;
-}
-*/
-
-
