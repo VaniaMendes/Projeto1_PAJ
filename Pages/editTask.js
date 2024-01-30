074e7d3bb22b8bb6a7d4f7ef843745d977410a9e
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-01-30 09:02:30 +0000

    Movimento botão Consultar

--- Pages/editTask.js
+++ Pages/editTask.js
@@ -8,41 +8,17 @@ document.getElementById("userHeader").innerHTML = "Bem vindo, " + username;
 // Carregar as tarefas existentes do localStorage, se existir um array senao cria um novo
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-const submitButton = document.getElementById("btn_submit");
-submitButton.onclick = add_task;
+const submitButton = document.getElementById("btn_edt");
+submitButton.onclick = edit_task;
 
 
 //Adiciona uma nova task
-function add_task(){
+function edit_task(){
 
-    const titleInput = document.getElementById('title1');
-    const descriptionInput = document.getElementById('description1');
+    const editTask = localStorage.getItem("editTask");
 
-    // Obtém os valores dos inputs
-    const title = titleInput.value;
-    const description = descriptionInput.value;
+    document.getElementById('title2').textContent = editTask.title;
+    document.getElementById('description2').textContent = editTask.description;
 
-    // Create a new task object
-    const newTask = {
-        id: tasks.length + 1,
-        title: title,
-        column: 'todo-cards',
-        description: description
-    };
-
-    //Adicionar a nova tarefa ao array
-    tasks.push(newTask);
-    newTask.column ='todo-cards';
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-
-    if(titleInput.value === ""){
-        alert("Por favor preencha o título.");
-    } 
-    else{
-        alert('Nova tarefa adicionada com sucesso: ' + '"'  + newTask.title + '"');
-    }
-
-    // Limpar os campos após adicionar uma nova tarefa
-    titleInput.value = "";
-    descriptionInput.value = "";
+    document.getElementById("btn_edt").textContent = "Gravar";
 }
--- Pages/scrum.js
+++ Pages/scrum.js
@@ -1,4 +1,3 @@
-
 // Get the username from local storage
 const username = localStorage.getItem("username");
 
@@ -9,101 +8,125 @@ const btn_in = document.getElementById("btn_inicio");
 btn_in.onclick = homeMenu;
 
 // When press "btn_inicio"
-function homeMenu(){
-    localStorage.removeItem("username");
-    document.location.href = '../index.html';
-}  
+function homeMenu() {
+  localStorage.removeItem("username");
+  document.location.href = "../index.html";
+}
 
-window.onload = () =>{
+// Consult task function
+function consultTask(title) {
+  // Set the 'editTask' key in localStorage with the task title to be consulted
+  localStorage.setItem("editTask", title);
 
-    // Carregar as tarefas existentes do localStorage, se houver
-    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  // Log to the console to check if the function is being called
+  console.log("Consulting task:", title);
 
-    // Listar as tarefas nos quadros
-    showTasks();
+  // Navigate to 'editTask.html'
+  document.location.href = "editTask.html";
+}
 
-    // Função para listar as tarefas nos quadros
-    function showTasks() {
-        // Limpar os quadros antes de listar novamente
-        document.getElementById("todo-cards").innerHTML = "";
-        document.getElementById("doing-cards").innerHTML = "";
-        document.getElementById("done-cards").innerHTML = "";
+window.onload = () => {
+  // Carregar as tarefas existentes do localStorage, se houver
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-        // Iterar sobre as tarefas e adicioná-las aos quadros apropriados
-        for (const t of tasks){
-        
-            const cardElement = createCardElement(t.title);
-            const columnElement = document.getElementById(t.column);
-            columnElement.appendChild(cardElement);
-        }
+  // Listar as tarefas nos quadros
+  showTasks();
+
+  // Função para listar as tarefas nos quadros
+  function showTasks() {
+    // Limpar os quadros antes de listar novamente
+    document.getElementById("todo-cards").innerHTML = "";
+    document.getElementById("doing-cards").innerHTML = "";
+    document.getElementById("done-cards").innerHTML = "";
+
+    // Iterar sobre as tarefas e adicioná-las aos quadros apropriados
+    for (const t of tasks) {
+      const cardElement = createCardElement(t.title);
+      const columnElement = document.getElementById(t.column);
+      columnElement.appendChild(cardElement);
+    }
+  }
+
+  // Função para criar um elemento de cartão HTML para uma tarefa
+  function createCardElement(title) {
+    const cardElement = document.createElement("div");
+    cardElement.className = "card";
+    cardElement.draggable = true;
+    const cardHeaderElement = document.createElement("div");
+    cardHeaderElement.className = "card-header";
+    cardHeaderElement.textContent = title;
+
+    // Adicionar evento de clique para exibir opções
+    cardHeaderElement.addEventListener("click", function () {
+      showOptions(cardElement);
+    });
+
+    cardElement.appendChild(cardHeaderElement);
+
+    return cardElement;
+  }
+
+  // Adicionar um event listener para fechar as opções quando clicar fora delas
+  document.addEventListener("click", function closeOptions(event) {
+    const optionsContainer = document.querySelector(".task-options");
+    if (optionsContainer && !optionsContainer.contains(event.target)) {
+      optionsContainer.remove();
+      document.removeEventListener("click", closeOptions);
+    }
+  });
+
+  function showOptions(cardElement) {
+    // Verificar se já há opções exibidas, se sim, remover
+    const existingOptions = cardElement.querySelector(".task-options");
+    if (existingOptions) {
+      existingOptions.remove();
+      return;
     }
 
-    // Função para criar um elemento de cartão HTML para uma tarefa
-    function createCardElement(title) {
-        const cardElement = document.createElement("div");
-        cardElement.className = "card";
-        cardElement.draggable = true;
-        const cardHeaderElement = document.createElement("div");
-        cardHeaderElement.className = "card-header";
-        cardHeaderElement.textContent = title;
+    // Criar opcoes de tarefa
+    const optionsContainer = document.createElement("div");
+    optionsContainer.className = "task-options";
+    optionsContainer.innerHTML = `
+      <button onclick="consultTask('${
+        cardElement.querySelector(".card-header").textContent
+      }')">Consultar</button>
+      <button onclick="deleteTask('${
+        cardElement.querySelector(".card-header").textContent
+      }')">Apagar</button>
+      <button onclick="moveTask('${
+        cardElement.querySelector(".card-header").textContent
+      }')">Mover</button>
+    `;
 
-        // Adicionar evento de clique para exibir opções
-        cardHeaderElement.addEventListener("click", function () {
-            showOptions(cardElement);
-        });
+    // Adicionar opções de tarefa à página
+    cardElement.appendChild(optionsContainer);
 
-        cardElement.appendChild(cardHeaderElement);
+    // Adicionar um event listener para fechar as opções quando clicar fora delas
+    document.addEventListener("click", function closeOptions(event) {
+      if (!cardElement.contains(event.target)) {
+        optionsContainer.remove();
+        document.removeEventListener("click", closeOptions);
+      }
+    });
+  }
 
-        return cardElement;
+  function deleteTask(title) {
+    // Encontrar a tarefa com o título correspondente na lista de tarefas
+    const taskIndex = tasks.findIndex((task) => task.title === title);
+
+    // Verificar se a tarefa foi encontrada
+    if (taskIndex !== -1) {
+      // Remover a tarefa da lista
+      tasks.splice(taskIndex, 1);
+
+      // Atualizar a exibição dos quadros
+      showTasks();
+
+      // Atualizar o localStorage
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      alert("Tarefa eliminada com sucesso" + title);
+    } else {
+      alert("Tarefa não encontrada");
     }
-
-    function showOptions(cardElement) {
-        // Verificar se já há opções exibidas, se sim, remover
-        const existingOptions = cardElement.querySelector('.task-options');
-        if (existingOptions) {
-            existingOptions.remove();
-            return;
-        }
-
-        // Criar opcoes de tarefa
-        const optionsContainer = document.createElement('div');
-        optionsContainer.className = "task-options";
-        optionsContainer.innerHTML = `
-            <button onclick="consultTask('${cardElement.querySelector(".card-header").textContent}')">Consultar</button>
-            <button onclick="deleteTask('${cardElement.querySelector(".card-header").textContent}')">Apagar</button>
-            <button onclick="moveTask('${cardElement.querySelector(".card-header").textContent}')">Mover</button>
-        `;
-
-        // Adicionar opções de tarefa à página
-        cardElement.appendChild(optionsContainer);
-
-        // Adicionar um event listener para fechar as opções quando clicar fora delas
-        document.addEventListener("click", function closeOptions(event) {
-            if (!cardElement.contains(event.target)) {
-                optionsContainer.remove();
-                document.removeEventListener("click", closeOptions);
-            }
-        });
-    }
-    
-    function deleteTask(title) {
-        // Encontrar a tarefa com o título correspondente na lista de tarefas
-        const taskIndex = tasks.findIndex(task => task.title === title);
-    
-        // Verificar se a tarefa foi encontrada
-        if (taskIndex !== -1) {
-            // Remover a tarefa da lista
-            tasks.splice(taskIndex, 1);
-    
-            // Atualizar a exibição dos quadros
-            showTasks();
-    
-            // Atualizar o localStorage
-            localStorage.setItem("tasks", JSON.stringify(tasks));
-            alert('Tarefa eliminada com sucesso' + title)
-        } else {
-            alert("Tarefa não encontrada");
-        }
-    }
-
+  }
 };
