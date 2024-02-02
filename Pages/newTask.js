8f8488731e4b3d1f1391264d21be6295e97564f6
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-02-02 13:07:18 +0000

    Revisão geral aos comentários

--- Pages/newTask.js
+++ Pages/newTask.js
@@ -4,20 +4,20 @@ const username = localStorage.getItem("username");
 // Atualizar a mensagem de boas vindas com o nome de utilizador
 document.getElementById("userHeader").innerHTML = "Bem vindo, " + username;
 
-// Carregar as tarefas existentes do armazenamento local, se existir um array senao cria um novo
+// Carregar as tarefas existentes do armazenamento local, se existir um array senão cria um novo
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-//Adiciona um  event listenner ao botao Adicionar Tarefa
+// Adiciona um Event Listener ao botao Adicionar Tarefa
 const submitButton = document.getElementById("newTask_btn_submit");
 submitButton.onclick = addTask;
 
-//Criar uma nova tarefa
+// Criar uma nova tarefa
 function addTask() {
-  // Obtém os valores dos inputs
+  // Declara e atribui variáveis aos elementos do título e descrição da tarefa
   const titleInput = document.getElementById("newTask_title");
   const descriptionInput = document.getElementById("newTask_description");
 
-  // Verifica tamanho máximo de caracteres do Título
+  // Verifica tamanho máximo de caracteres do título
   const maxLength = 50;
   let title;
   if (titleInput.value.length > maxLength) {
@@ -30,16 +30,16 @@ function addTask() {
   }
   let description = descriptionInput.value;
 
-  // Verifica se o título já existe em alguma tarefa, devolve um boolean
+  // Verifica se o título já existe em alguma tarefa, através do método some() e devolve um boolean
   const existentTitle = tasks.some((task) => task.title === title);
 
   if (titleInput.value === "") {
     alert("Por favor preencha o título.");
-    //Verifica se o titulo ja existe
+    // Protege criação de tarefas com o mesmo título
   } else if (existentTitle) {
     alert("Já existe uma tarefa com esse título.");
   } else {
-    // se o titulo ainda não existir cria uma nova tarefa
+    // Cria uma nova tarefa com os atributos title, column e description. Todas as tarefas começam na coluna TODO
     const newTask = {
       title: title,
       column: "todo-cards",
@@ -47,7 +47,7 @@ function addTask() {
     };
     // Adicionar a nova tarefa ao array
     tasks.push(newTask);
-    //Grava as alterações no local Storage
+    // Grava as alterações no armazenamento local
     localStorage.setItem("tasks", JSON.stringify(tasks));
     alert("Nova tarefa adicionada com sucesso!");
   }
